564e34b74a9afcf7402a7f98bf5fbd4d77c2a5f9
Matthew Reilly <matthewfreilly@gmail.com>
2020-07-13 02:47:33 -0500

    stuck on sqlachemy binds not set

--- app.py
+++ app.py
@@ -11,20 +11,13 @@ from flask_sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from sqlalchemy.dialects.postgresql import JSON
+from psycopg2.extensions import register_adapter
 
-
-app = Flask(__name__)
-app.config.from_object(os.environ.get('APP_SETTINGS'))
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-
-q = Queue(connection=conn)
-
-from models import *
-
+register_adapter(dict, JSON)
 
 def count_and_save_words(url):
-
+    
     errors = []
 
     try:
@@ -36,7 +29,7 @@ def count_and_save_words(url):
         return {"error": errors}
 
     # text processing
-    raw = BeautifulSoup(r.text, 'html.parser').get_text()
+    raw = BeautifulSoup(r.text).get_text()
     nltk.data.path.append('./nltk_data/')  # set the path
     tokens = nltk.word_tokenize(raw)
     text = nltk.Text(tokens)
@@ -45,11 +38,11 @@ def count_and_save_words(url):
     nonPunct = re.compile('.*[A-Za-z].*')
     raw_words = [w for w in text if nonPunct.match(w)]
     raw_word_count = Counter(raw_words)
-
+    print(raw_words)
     # stop words
     no_stop_words = [w for w in raw_words if w.lower() not in stops]
     no_stop_words_count = Counter(no_stop_words)
-
+    print("HERE IS DB JUST PRIOR TO INIT SESSION", db)
     # save the results
     try:
         result = Result(
@@ -57,6 +50,8 @@ def count_and_save_words(url):
             result_all=raw_word_count,
             result_no_stop_words=no_stop_words_count
         )
+        print(result)
+        print("HERE IS DB JUST PRIOR TO INIT SESSION", db)
         db.session.add(result)
         db.session.commit()
         return result.id
@@ -65,14 +60,44 @@ def count_and_save_words(url):
         return {"error": errors}
 
 
+
+R=0
+app = Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+q = Queue(connection=conn)
+print("HERE IS DB JUST PRIOR TO INIT SESSION", db)
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String())
+    result_all = db.Column(JSON)
+    result_no_stop_words = db.Column(JSON)
+
+    def __init__(self, url, result_all, result_no_stop_words):
+        
+        self.url = url
+        self.result_all = result_all
+        self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     results = {}
     if request.method == "POST":
+        # this import solves a rq bug which currently exists
+        from app import count_and_save_words
+
         # get url that the person has entered
         url = request.form['url']
-        if 'http://' not in url[:7]:
-            url = 'http://' + url
+        if not url[:8].startswith(('https://', 'http://')):
+            url = 'https://' + url
         job = q.enqueue_call(
             func=count_and_save_words, args=(url,), result_ttl=5000
         )
@@ -80,22 +105,30 @@ def index():
 
     return render_template('index.html', results=results)
 
-
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
 
     job = Job.fetch(job_key, connection=conn)
-
+    
+    print("THIS IS CONN=>=>=>", conn)
     if job.is_finished:
+        print(R)
         result = Result.query.filter_by(id=job.result).first()
         results = sorted(
             result.result_no_stop_words.items(),
             key=operator.itemgetter(1),
             reverse=True
         )[:10]
+        print(jsonify(results))
         return jsonify(results)
+       
     else:
         return "Nay!", 202
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -2,12 +2,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'SECRET_KEY'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+   
 
 class ProductionConfig(Config):
     DEBUG = False
